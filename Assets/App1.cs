637d272e334e6e1deee61a42641e51c7fc189cf2
nina <ninab@artrebel9.com>
2018-10-09 16:40:42 +0200

    Initial commit

---
+++ Assets/App1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class App1 : MonoBehaviour
+{
+    public Text TextBlock1;
+
+    private void Start()
+    {
+        var clickStream = Observable.EveryUpdate()
+                .Where(_ => Input.GetMouseButtonDown(0));
+
+            clickStream.Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
+                .Where(xs => xs.Count >= 2)
+				.SubscribeToText(TextBlock1, xs => "Number of fast shots: " + xs.Count);
+        
+    }
+}
---
+++ Assets/AppInterval.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+
+public class AppInterval : MonoBehaviour {
+
+	public GameObject Cube;
+    private IDisposable _update;	//intervale ne shranjujemo
+
+    void Start()
+    {
+        _update = Observable.Interval(TimeSpan.FromMilliseconds(1000)).Subscribe(x => 
+		// x starts from 0 and is incremented everytime the stream pushes another item.
+        {
+            Cube.transform.rotation = UnityEngine.Random.rotation;
+        });
+    }	//naredimo Observable _update, na katerem imamo interval, na katerega se povežemo in nam da stream vsakih 1000ms
+		//takrat naredimo rotacijo kocke
+
+    private void OnDestroy()
+    {
+        _update.Dispose();	//ko ustavimo program, zavržemo vse intervale
+    }
+}
